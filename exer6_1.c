b21982fb34986f371ead4bb043ff5e9a0e975eac
radsl.eng@uea.edu.br <rosemiro@DESKTOP-46CNV2M.localdomain>
2022-08-29 12:48:32 -0400

    os exercicios1

---
+++ exer6_1.c
@@ -0,0 +1,26 @@
+/*
+	Rosemiro Andrade Da Silva-1515180065
+	Mateus Mamede da Silva Marques-2015180069
+*/
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+        int a,b;
+        printf("digite um numero:\n");
+        scanf("%d",&a);
+
+        printf("digite outro valor:\n");
+        scanf("%d",&b);
+
+        if (a>b)
+        {
+                printf("o maior valor de:%d\n",a);
+        }
+        else
+        {
+                printf("o maior valor e:%d\n",b);
+        }
+
+        return 0;
+}
